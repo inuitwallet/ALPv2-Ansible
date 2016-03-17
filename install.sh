02f47989ed7fa204ef1d698e666897e8b64db82f
sammoth <sam.griffiths@wazoku.com>
2016-03-17 06:06:16 +0000

    update shell script

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 echo updating
 sudo apt-get update
 
-echo installing add-apt-repository utility
-sudo apt-get install software-properties-common
+echo installing git and add-apt-repository utility
+sudo apt-get install git software-properties-common
 
 echo adding ansible PPA repository
 sudo apt-add-repository ppa:ansible/ansible
@@ -14,6 +14,12 @@ sudo apt-get update
 
 echo installing ansible 2
 sudo apt-get install ansible
+
+echo fetching ansible repo
+git clone http://github.com/inuitwallet/ALPv2-Ansible
+
+echo moving to the Ansible directory
+cd ALPv2-Ansible
 
 echo running Ansible playbook to install ALPv2
 ansible-playbook playbook.yml -K
