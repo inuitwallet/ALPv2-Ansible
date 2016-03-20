89357af8e1b0051705ad1bfe0f9518df84984db1
sammoth <sam.griffiths@wazoku.com>
2016-03-20 20:30:50 +0000

    update apt-get commands to autommaticallly ugrade and install

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ echo updating
 sudo apt-get update
 
 echo installing git and add-apt-repository utility
-sudo apt-get install git software-properties-common
+sudo apt-get install -y git software-properties-common
 
 echo adding ansible PPA repository
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 
 echo updating
 sudo apt-get update
 
 echo installing ansible 2
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
 echo fetching ansible repo
 git clone http://github.com/inuitwallet/ALPv2-Ansible
